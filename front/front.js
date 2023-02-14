59e34e1af2c03651c4b929d81fb6dad9b0ad114e
Simo Sirén <simo.siren1@careeria.fi>
2023-02-14 12:48:25 +0200

    valmis kai

--- front/front.js
+++ front/front.js
@@ -1,6 +1,22 @@
 
 function getProducts() {
-    fetch('http://localhost:3300/api/products') // tehdään http get pyyntö
-    .then(res => res.json()) // muutetaan palvelimen vastaus javascript muotoon
-    .then(data => alert("Procuct name: " + data[0].name + ", price: " + data[0].price))
+
+    document.getElementById("taulukko").innerHTML = "<h4>Ladataan...</h4>"
+
+let x = "<table><thead><th>Product name</th><th>Price</th></thead><tbody>"
+
+fetch('http://localhost:3300/api/products')
+  .then(jsonResponse => jsonResponse.json()) // muutetaan json data javascript muotoon
+  .then(oliot => oliot.map(prod => { // loopataan läpi oliot
+    x += `<tr><td>${prod.name}</td><td>${prod.price}</td></tr>`
+    }
+  ))
+
+  setTimeout(() => {
+    x += `</tbody></table>`
+  document.getElementById("sisältö").innerHTML = x}
+  , 500 )
+
 }
+// C# esimerkki template stringistä (string interpolation):
+// $"Ongelma havaittu lisättäessä tuotetta {product.name}"
