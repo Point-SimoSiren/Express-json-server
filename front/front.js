ae8abd001af5c9b18b4d86d8144207c95e6023b5
Simo Sirén <simo.siren1@careeria.fi>
2023-02-14 11:31:56 +0200

    melkein valmis2

--- front/front.js
+++ front/front.js
@@ -1,6 +1,6 @@
 
 function getProducts() {
     fetch('http://localhost:3300/api/products') // tehdään http get pyyntö
-    .then(res => res.json) // muutetaan palvelimen vastaus javascript muotoon
-    .then(data => alert(data))
+    .then(res => res.json()) // muutetaan palvelimen vastaus javascript muotoon
+    .then(data => alert("Procuct name: " + data[0].name + ", price: " + data[0].price))
 }
